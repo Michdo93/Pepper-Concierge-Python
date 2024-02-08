a6f766833f73a9b0d4663501b21018547a6ff4b5
Michdo93 <michaeldoerflinger93@gmail.com>
2024-02-08 12:12:26 +0100

    updated mqtt config

--- MQTTConenectionManager.py
+++ MQTTConenectionManager.py
@@ -5,27 +5,72 @@ import xml.etree.ElementTree as ET
 class MQTTConnectionManager:
     def __init__(self, delegate):
         self.delegate = delegate
-        self.qos = 0
-        self.auth = None
-        self.broker_tls = None
 
         config_file_path = "config/config.xml"
         root = ET.parse(config_file_path).getroot()
 
+        self.broker_transport = root.find('MQTT_BROKER_TRANSPORT').text
+        self.broker_ip = root.find('MQTT_BROKER_IP').text
+        self.broker_port = int(root.find('MQTT_BROKER_PORT').text)
+        self.client_id = root.find('MQTT_CLIENT_ID').text
+        self.tls_path = root.find('MQTT_BROKER_IP').text
+        self.tls_version = int(root.find('MQTT_TLS_VERSION').text)
+        self.broker_user = root.find('MQTT_BROKER_USER').text
+        self.broker_password = root.find('MQTT_BROKER_PASSWORD').text
+        self.broker_qos = int(root.find('MQTT_BROKER_QOS').text)
+        self.retain = self.__stringToBoolean(root.find('MQTT_RETAIN').text)
+        self.broker_async = self.__stringToBoolean(root.find('MQTT_BROKER_ASYNC').text)
+
+        self.auth = None
+
         self.topic_publish_base = root.find('MQTT_PUBLISH_TOPIC_BASE').text
         self.topic_subscribe_base = root.find('MQTT_SUBSCRIBE_TOPIC_BASE').text
-        self.broker_ip = root.find('MQTT_BROKER_IP').text
-        self.broker_port = int(root.find('MQTT_BROKER_PORT').text)  # Convert port to integer
-        self.brokerTransport = "tcp"
 
-        self.client_id = "Pepper"
-        self.client = mqtt.Client(self.client_id, clean_session=True,userdata=None, protocol=mqtt.MQTTv311, transport=self.brokerTransport)
+        if self.broker_qos not in range(0, 3):
+            self.broker_qos = 0
+
+        if self.broker_user is not None:
+            if self.broker_password is not None:
+                self.auth = {'username': self.broker_user,
+                             'password': self.broker_password}
+            else:
+                self.auth = {'username': self.broker_user,
+                             'password': ""}
+        else:
+            self.auth = None
+
+        if self.broker_user == "" or self.broker_user == None:
+            self.auth = None
+
+        if self.broker_port != 1883:
+            if self.tls_path is not None or self.tls_path != "":
+                if self.tls_version is not None or self.tls_version != "":
+                    self.broker_tls = (self.tls_path, self.tls_version)
+                else:
+                    self.broker_tls = self.tls_path
+            else:
+                self.broker_tls = None
+
+        self.client = mqtt.Client(self.client_id, clean_session=True, userdata=None, protocol=mqtt.MQTTv311, transport=self.broker_transport)
+
+        if self.broker_tls is not None:
+            self.client.tls_set(self.broker_tls)
+
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
         self.client.username_pw_set(self.auth)
         
         # Jetzt können Sie die Verbindung zum Broker herstellen
-        self.client.connect(self.broker_ip, self.broker_port)
+        if self.broker_async:
+            self.client.connect_async(self.broker_ip, self.broker_port)
+        else:
+            self.client.connect(self.broker_ip, self.broker_port)
+
+    def __stringToBoolean(self, string):
+        if string.lower() == "true":
+            return True
+        else:
+            return False
 
     def on_connect(self, client, userdata, flags, rc):
         if rc == 0:
@@ -42,9 +87,11 @@ class MQTTConnectionManager:
 
     def publish_to_item(self, item, payload):
         topic = self.topic_publish_base + str(item)
-        print("Publishing to topic:", topic)
-        print("Payload:", payload)
-        self.client.publish(topic, payload, self.qos)
+
+        if self.retain:
+            self.client.publish(topic, payload, self.qos, retain=self.retain)
+        else:
+            self.client.publish(topic, payload, self.qos)
 
     def subscribe_to_item(self, item):
         topic = self.topic_subscribe_base + str(item)
